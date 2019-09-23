806dca674ea9497904e8552427fe096a6d323240
Guillaume <liard.guillaume@gmail.com>
2019-09-23 20:19:17 +0100

    Feature test added for the program

---
+++ spec/accountFeatureSpec.js
@@ -0,0 +1,28 @@
+describe('Feature - Account', () => {
+  dateToString = require("./helpers/dateHelper");
+  
+  const Account = require('../lib/account');
+  const today = dateToString(new Date());
+  
+  it('allows a user to use a bank account', () => {
+    const account = new Account;
+    account.deposit(100);
+    account.withdraw(10);
+    account.withdraw(40);
+    account.withdraw(15.6);
+    account.deposit(50);
+
+    expect(() => {account.withdraw(150)}).toThrowError("Funds insufficient!");
+
+    spyOn(console, 'log');
+    account.statement()
+    
+    expect(console.log.calls.argsFor(0)[0]).toEqual("date || credit || debit || balance");
+    expect(console.log.calls.argsFor(1)[0]).toEqual(`${today} || 50.00 ||   || 84.40`);
+    expect(console.log.calls.argsFor(2)[0]).toEqual(`${today} ||   || 15.60 || 34.40`);
+    expect(console.log.calls.argsFor(3)[0]).toEqual(`${today} ||   || 40.00 || 50.00`);
+    expect(console.log.calls.argsFor(4)[0]).toEqual(`${today} ||   || 10.00 || 90.00`);
+    expect(console.log.calls.argsFor(5)[0]).toEqual(`${today} || 100.00 ||   || 100.00`);
+  });
+  
+});
---
+++ spec/helpers/dateHelper.js
@@ -0,0 +1,9 @@
+function dateToString(date) {
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  return day + "/" + month + "/" + year
+}
+
+module.exports = dateToString;
+
